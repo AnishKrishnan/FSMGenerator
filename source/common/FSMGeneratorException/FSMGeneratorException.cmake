3d9f9a120122dd7264d21a6b618c17f767382bc0
Anish Krishnan <anish.krishnan2708@gmail.com>
2015-12-07 23:15:27 -0800

    fixed bug to allow for proper test run

--- source/common/FSMGeneratorException/FSMGeneratorException.cmake
+++ source/common/FSMGeneratorException/FSMGeneratorException.cmake
@@ -4,10 +4,10 @@
 include_directories(${CMAKE_CURRENT_LIST_DIR})
 
 # Source files
-FILE(GLOB SOURCES ${CMAKE_CURRENT_LIST_DIR}/*.cpp)
+FILE(GLOB SRC ${CMAKE_CURRENT_LIST_DIR}/*.cpp)
 
 ##### Outputs #####
 
 # Create emlib library
-add_library(FSMGeneratorException ${SOURCES})
+add_library(FSMGeneratorException ${SRC})
 set(LIBS ${LIBS} FSMGeneratorException)

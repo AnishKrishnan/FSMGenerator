1c038b4faa372034a845799236c086503e6c3663
Anish Krishnan <anish.krishnan2708@gmail.com>
2015-12-08 22:09:24 -0800

    added ConfigManager to cmake

--- source.cmake
+++ source.cmake
@@ -2,4 +2,5 @@
 set(SOURCE_DIR ${CMAKE_CURRENT_LIST_DIR}/source)
 set(COMMON_DIR ${SOURCE_DIR}/common)
 
+include(${COMMON_DIR}/ConfigManager/ConfigManager.cmake)
 include(${COMMON_DIR}/FSMGeneratorException/FSMGeneratorException.cmake)

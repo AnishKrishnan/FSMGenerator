cc8cadf52df4d2c3a785421846e93edf405d8af2
Anish Krishnan <anish.krishnan2708@gmail.com>
2015-12-19 15:26:35 -0800

    created mocks for file writer interface

---
+++ test/mocks/MockFileWriter.h
@@ -0,0 +1,21 @@
+#ifndef MockFileWriter_H
+#define MockFileWriter_H
+
+#include "IFileWriter.h"
+#include "gmock/gmock.h"
+
+class MockFileWriter : public IFileWriter
+{
+public:
+	 MOCK_METHOD1(Write, void(const char * pData));
+
+	 MOCK_METHOD1(Write, void(const std::string & pData));
+	
+	 MOCK_METHOD1(WriteLine, void(const char * pData));
+	
+	 MOCK_METHOD1(WriteLine, void(const std::string & pData));
+
+	 MOCK_METHOD0(Flush, void());
+};
+
+#endif //MockFileWriter_H

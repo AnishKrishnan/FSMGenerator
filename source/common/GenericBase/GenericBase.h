e650262bc767ddecfd5cf1eb8ca6fc03318b68d2
Anish Krishnan <anish.krishnan@gmail.com>
2016-01-03 13:32:35 +1300

    Created skeleton for generic base

--- source/common/GenericBase/GenericBase.h
+++ source/common/GenericBase/GenericBase.h
@@ -1,37 +1,27 @@
 /** \addtogroup Common
  *  @{
- * Custom exception class to use for the FSMGenerator Project
+ * Custom base class to be used for all other classes
  */
 
-#ifndef FSMGeneratorException_H
-#define FSMGeneratorException_H
+#ifndef GenericBase_H
+#define GenericBase_H
 
-#include "stdexcept"
+#include "ConfigManager.h"
+#include "Logger.h"
+#include "StdOutFileWriter.h"
 
-class FSMGeneratorException : public std::runtime_error
+class GenericBase
 {
+private:
+	Logger * _logger;
+	StdOutFileWriter _output;
+	ConfigManager * _configManager;
+
 public:
 
-	/**
-	 * @brief Constructor for FSMGeneratorException
-	 * @details Default constructor which results in empty message
-	 */
-	FSMGeneratorException();
-
-	/**
-	 * @brief Constructor for FSMGeneratorException with const char message
-	 * 
-	 * @param pMessage Exception message
-	 */
-	explicit FSMGeneratorException(const char * pMessage);
-
-	/**
-	 * @brief Constructor for FSMGeneratorException with std::string message
-	 * 
-	 * @param pMessage Exception Message
-	 */
-	explicit FSMGeneratorException(const std::string & pMessage);
+	GenericBase ();
+	
 };
 
-#endif //FSMGeneratorException_H
+#endif //GenericBase_H
 /** @}*/

a74a40ec68d9df3a15dbecb1821420a48ee92c00
Anish Krishnan <anish.krishnan2708@gmail.com>
2016-01-03 16:10:30 -0800

    created IFileReader interface

---
+++ source/interfaces/IFileReader.h
@@ -0,0 +1,23 @@
+/** \addtogroup Interfaces
+ *  @{
+ * interface for file reader class
+ */
+ 
+#ifndef IFileReader_H
+#define IFileReader_H
+
+class IFileReader
+{
+public:
+	virtual ~IFileReader() {}
+	
+	/**
+	 * @brief Determines if the file is open
+	 * @return bool indicating if the file is open
+	 */
+	virtual bool IsOpen (void) = 0;
+};
+
+#endif //IFileReader_H
+
+ /** @}*/
